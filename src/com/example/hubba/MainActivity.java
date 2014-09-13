1151f6f68a6cd67fa9d1465aff72139902fe4fdd
MichaelGSnook <Owner@michaelsmbp230.student.iastate.edu>
2014-09-13 13:49:27 -0500

    Reestablished Repo
    Added “dock” on MainActivity. Created activities for Twitter, Facebook,
    and instagram.

---
+++ src/com/example/hubba/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.hubba;
+
+import android.view.View;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+    public void goInsta(View view) {
+		startActivity(new Intent(this, INSTAsecond_screen.class)); 
+	}
+    
+    public void goTwit(View view) {
+		startActivity(new Intent(this, Twitsecond_screen.class)); 
+	}
+    
+    public void goFB(View view) {
+		startActivity(new Intent(this, FBsecond_screen.class)); 
+    }
+}
---
+++ src/com/example/hubba/Twitsecond_screen.java
@@ -0,0 +1,15 @@
+package com.example.hubba;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Twitsecond_screen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_twitsecond_screen);
+	}
+}
